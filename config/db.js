22b8bf0440262ef338c10c1e5d6e4127683623c0
Akshar Ambaliya <137415917+akshar811@users.noreply.github.com>
2024-09-21 05:36:34 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/Tak_Management_App");
+  await mongoose.connect("mongodb://127.0.0.1:27017/Tak_Management_App_enhancement");
   console.log("connect database");
 };
 
