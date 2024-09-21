866d1160e01aaef247a7af92afd300939368922c
Akshar Ambaliya <137415917+akshar811@users.noreply.github.com>
2024-09-21 05:37:45 +0530

    Update Task_Schema.js

--- models/Task_Schema.js
+++ models/Task_Schema.js
@@ -3,18 +3,25 @@ const mongoose = require("mongoose");
 const TaskSchema = new mongoose.Schema(
   {
     title: String,
-    des: String,
+    des:  String,
     category: String,
     userID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     duedate: { type: Date },
     assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    status: {
+      type: String,
+      enum: ["Pending", "In Progress", "Completed"],
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+    },
     reviews: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "comments",
       },
     ],
-    
   },
   { timestamps: true }
 );
