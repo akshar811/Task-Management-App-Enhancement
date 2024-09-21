162086deaa016d02edc6dd896bd108286c1fa457
Akshar Ambaliya <137415917+akshar811@users.noreply.github.com>
2024-09-21 05:38:15 +0530

    Update task_route.js

--- routes/task_route.js
+++ routes/task_route.js
@@ -1,19 +1,43 @@
 const { Router } = require("express");
-const { Addtask, Tasks, DisplayTask, deleteTask, AdminTaskDisplay, searchTasks, updateTask, singletask, home, reviews } = require("../controllers/Task_controller");
+const {
+  Addtask,
+  Tasks,
+  DisplayTask,
+  deleteTask,
+  AdminTaskDisplay,
+  searchTasks,
+  updateTask,
+  singletask,
+  home,
+  reviews,
+  getTasksWithPagination,
+  getTasks,
+  exportTasksToCSV,
+  importTasksFromCSV,
+  upload,
+} = require("../controllers/Task_controller");
 const { Auth } = require("../middleware/Auth");
 
 const TaskRoute = Router();
 
-TaskRoute.get("/Addtask",Auth,Addtask);
-TaskRoute.post("/Addtasks",Auth,Tasks);
-TaskRoute.get("/DisplayTask",Auth,DisplayTask);
-TaskRoute.delete("/DeleteTask/:id",deleteTask);
-TaskRoute.get("/AdminTaskDisplay",AdminTaskDisplay);
-TaskRoute.get("/searchTasks",searchTasks);
-TaskRoute.post("/updateTask",updateTask);
-TaskRoute.get("/singleTask/:id",singletask);
-TaskRoute.get("/Home",home);
+TaskRoute.get("/Addtask", Auth, Addtask);
+TaskRoute.post("/Addtasks", Auth, Tasks);
+TaskRoute.get("/DisplayTask", Auth, DisplayTask);
+TaskRoute.delete("/DeleteTask/:id", deleteTask);
+TaskRoute.get("/AdminTaskDisplay", AdminTaskDisplay);
+TaskRoute.get("/searchTasks", searchTasks);
+TaskRoute.post("/updateTask", updateTask);
+TaskRoute.get("/singleTask/:id", singletask);
+TaskRoute.get("/Home", home);
 
-TaskRoute.post("/:id/comment",Auth ,reviews);
+TaskRoute.post("/:id/comment", Auth, reviews);
+
+TaskRoute.get("/pagination", getTasksWithPagination);
+
+TaskRoute.get("/filter", getTasks);
+
+TaskRoute.get("/exportTasks", exportTasksToCSV);
+
+TaskRoute.post("/importTasks", upload.single("csvfile"), importTasksFromCSV);
 
 module.exports = TaskRoute;
